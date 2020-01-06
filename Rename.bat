7652ffb585e3af5f7f9c67d5efaba7231aa1b5d3
Shaul Fridman <shaul.fridman@gmail.com>
2020-01-06 11:26:38 +0200

    (+) Script to rename the lib

---
+++ Rename.bat
@@ -0,0 +1,4 @@
+gsar -o -s_foo -r_oof RenameSym_1.lib RenameSym_1.lib
+gsar -o -s??3@YAXPAXI@Z -r_IAmOpDelete_ RenameSym_1.lib 
+gsar -o -s??2@YAPAXI@Z -r_IAmOpNew___ RenameSym_1.lib 
+rem gsar -o -s??2@YAPAXIPAX@Z -r_IAmOpPlcmntNew RenameSym_1.lib 
